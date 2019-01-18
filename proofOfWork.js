25c88b93dfae8a08e8341ce73b72770be13a1b9d
Simon Mullaney <simonpmullaney@gmail.com>
2019-01-18 16:44:17 +0000

    First commit

---
+++ proofOfWork.js
@@ -0,0 +1,32 @@
+const crypto = require('crypto');
+
+var difficulty = '000'
+
+function randomString() {
+ let longString = '';
+ for (let i = 0; i < 1000000; i += 1) {
+  longString += Math.random().toString(36).substr(2, 1);
+ }
+ return longString;
+}
+
+
+(function doWork() {
+     const challengeString = randomString();
+     let nonce = 0;
+     const startDate = new Date();
+     let hash = crypto.createHash('sha256').update(challengeString + nonce).digest('hex');
+
+
+     console.log(difficulty)
+
+     while (hash.substr(0, 3) !== difficulty) {
+        hash = crypto.createHash('sha256').update(challengeString + nonce).digest('hex');
+        console.log(nonce)
+        nonce += 1;
+     }
+     const endDate = new Date();
+     console.log('Found the answer');
+     console.log(`The answer is ${nonce} with a final hash of ${hash}`);
+     console.log(`${(endDate - startDate) / 1000} seconds to complete`);
+}());
